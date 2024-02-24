6920b54dcac43813d6c9934510aa1554af2940d9
Thayna <thayrcristina@gmail.com>
2024-02-24 16:32:12 -0300

    Estudos 24/02 - Encapsulamento

---
+++ src/encapsulamento/ControleDemo.java
@@ -0,0 +1,16 @@
+package encapsulamento;
+
+public class ControleDemo {
+    public static void main(String[] args) {
+
+
+    ControleRemoto c = new ControleRemoto();
+//    c.ligar();
+    c.maisVolume();
+    c.play();
+//    c.ligarMudo();
+    c.abrirMenu();
+    c.fecharMenu();
+
+    }
+}
