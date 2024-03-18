892c7ff8273ed8af82557c1bdc171d36ac117729
Thayna <thayrcristina@gmail.com>
2024-03-17 22:45:55 -0300

    Agregação de classes

--- src/relacionamento/UltraEmojiCombat.java
+++ src/relacionamento/UltraEmojiCombat.java
@@ -9,10 +9,12 @@ public class UltraEmojiCombat {
                 "França", 31, 1.75f, 68.9f,
                 11, 2, 1);
         l[1] = new Lutador("Putscript", "Brasil", 29,
-                1.68f, 10.8f, 14, 2, 3);
+                1.75f, 68.9f, 14, 2, 3);
 
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar();
         l[0].status();
         l[1].status();
-
     }
 }
